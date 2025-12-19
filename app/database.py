d6b66812303b50af48cc9843fbd1d78d62e91425
thibo <thibo.detremmerie@student.vives.be>
2025-12-19 16:50:53 +0100

    Creation Database file and location of the file inside the Data folder

--- app/database.py
+++ app/database.py
@@ -1,2 +1,44 @@
-# -*- coding: utf-8 -*-
+import sqlite3
+from config import settings
 
+
+class Database: 
+    def __init__(self, db_path=settings.DATABASE_PATH):
+        self.db_path = db_path
+        self.connection = None
+        self.create_tables()
+
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.db_path)
+        return self.connection
+    
+    def create_tables(self):
+        dbconnection = self.connect()
+        cursor = dbconnection.cursor()
+        
+    
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS practice_sessions (
+              id integer PRIMARY KEY AUTOINCREMENT,
+              data TEXT NOT NULL,
+              duration INTEGER NOT NULL,
+              warmup INTEGER,
+              exercises TEXT
+            )
+        ''')
+        
+        cursor.execute('''
+           CREATE TABLE IF NOT EXISTS goals (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             description TEXT NOT NULL,
+             completed BOOLEAN NOT NULL
+           )
+      ''')
+           
+        dbconnection.commit()
+        dbconnection.close()
+        
+if __name__ == "__main__":
+    db = Database()
+    print("Database + tables are created")
--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,1 @@
-# -*- coding: utf-8 -*-
-
+DATABASE_PATH = "../data/trumpet_practice.db"
