ca3d05c44d277d4613c3c9e4a2bbdeaa11466797
thibo <thibo.detremmerie@student.vives.be>
2025-12-22 15:06:07 +0100

    Making sure the csv files are added in the data folder, before they were added in the app folder

--- app/report.py
+++ app/report.py
@@ -1,18 +1,28 @@
 import csv 
 import pandas as pd
 from app.database import Database
+import os
+from config import settings
 
 def generate_csv_report():
     db = Database()
     sessions = db.fetch_all("SELECT * FROM practice_sessions")
     goals = db.fetch_all("SELECT * FROM goals")
     
-    with open('practice_sessions_report.csv', 'w', newline='') as f:
+    
+    # This will check if a data folder exists
+    data_dir = os.path.dirname(settings.DATABASE_PATH)
+    os.makedirs(data_dir, exist_ok=True)
+    
+    sessions_path = os.path.join(data_dir, "practice_sessions_report.csv")
+    goals_path = os.path.join(data_dir, "goals_report.csv")
+    
+    with open(sessions_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['id', 'date', 'duration','warmup', 'exercises'])
         writer.writerows(sessions)
         
-    with open('goals_report.csv', 'w', newline='') as f:
+    with open(goals_path, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['id', 'description', 'target', 'completed'])
         writer.writerows(goals)
