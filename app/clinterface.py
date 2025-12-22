f00d9a74a2bcef1ceebe056577a9aea7c5bca013
thibo <thibo.detremmerie@student.vives.be>
2025-12-22 14:01:47 +0100

    Further building the clientinterface.py file with a main class. Working with argparse

--- app/clinterface.py
+++ app/clinterface.py
@@ -1,5 +1,6 @@
 from app.database import Database
-
+import argparse
+from app.report import generate_csv_report, generate_excel_report
 
 
 def add_practice_sessions(date, duration, warmup, exercises):
@@ -27,3 +28,55 @@ def list_goals():
     goals = db.fetch_all("SELECT * FROM goals")
     for goal in goals:
         print(goal)
+
+
+def main():
+    parser = argparse.ArgumentParser(desciption="Trumpet Practice Tracker")
+    parser.add_argument('--add-practice', nargs=4, metavar=('date', 'duration', 'warmup', 'exercises'),
+                        help="Add a new practice session")
+    parser.add_argument('--add-goal', nargs=3, metavar=('description', 'target', 'completed'),
+                        help="Add a goal")
+    parser.add_argument('--list-practice', action='store_true', help="List all practice sessions")
+    parser.add_argument('--list-goals', action='store_true', help="List all goals")
+    parser.add_arguemtn('--generate-report-csv', action='store_true', help="Generate a CSV report")
+    parser.add_argument('--generate-report-excel', action='store_true', help="Generate an Excel report")
+    
+    args = parser.parse_args()
+    
+    if args.add_practice:
+        add_practice_sessions(*args.add_practice)
+    elif args.add_goal:
+        add_goal(*args.add_goal)
+    elif args.list_practice:
+        list_practice_sessions()
+    elif args.list_goals:
+        list_goals()
+    elif args.generate_report_csv:
+        generate_csv_report()
+    elif args.generate_report_excel:
+        generate_excel_report()
+    else:
+        print("No valid command.")
+        
+        
+        
+if __name__=='__main__':
+    main()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
