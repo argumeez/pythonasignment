40abc0d79322efca5500876c8462bc9d54471fff
thibo <thibo.detremmerie@student.vives.be>
2025-12-27 15:23:49 +0100

    Stop tracking settings.py (now ignored)

--- config/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-#this code finds the base directory of the project. Changes it to an absolute path, and moves 2 folders up. 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#Starts at BASE_DIR and goes into te data folder where it points to trumpet_practice.db
-DATABASE_PATH = os.path.join(BASE_DIR, "data", "trumpet_practice.db")
